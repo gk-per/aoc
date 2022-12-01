ea02677e8ac4f29754927945e297879161710fea
Christopher Stansbury <chris.stansbury@revelry.co>
2022-12-01 14:29:33 -0600

    remove enum.reverse from part_two and cleanup

--- day1/lib/day1.ex
+++ day1/lib/day1.ex
@@ -13,25 +13,23 @@ defmodule AdventOfCode.Day1 do
 
   """
   def part_one do
-    "lib/input.txt"
-    |> File.read!()
-    |> String.split("\n\n")
-    |> Enum.map(fn x -> String.split(x, "\n") end)
-    |> Enum.map(fn x -> Enum.map(x, fn y -> String.to_integer(y) end) end)
-    |> Enum.map(fn x -> Enum.sum(x) end)
+    collate_cookie_calorie_counts()
     |> Enum.max()
   end
 
   def part_two do
+    collate_cookie_calorie_counts()
+    |> Enum.sort(&(&1 > &2))
+    |> Enum.take(3)
+    |> Enum.sum()
+  end
+
+  defp collate_cookie_calorie_counts() do
     "lib/input.txt"
     |> File.read!()
     |> String.split("\n\n")
     |> Enum.map(fn x -> String.split(x, "\n") end)
     |> Enum.map(fn x -> Enum.map(x, fn y -> String.to_integer(y) end) end)
     |> Enum.map(fn x -> Enum.sum(x) end)
-    |> Enum.sort()
-    |> Enum.reverse()
-    |> Enum.take(3)
-    |> Enum.sum()
   end
 end
