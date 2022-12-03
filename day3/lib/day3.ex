829092bbc114a92810128c14b2f48583897e0097
Christopher Stansbury <christansbur@gmail.com>
2022-12-03 14:02:16 -0600

    better score calculation (lifted from elixir forum)

--- day3/lib/day3.ex
+++ day3/lib/day3.ex
@@ -25,7 +25,7 @@ defmodule AdventOfCode.Day3 do
 
     Enum.zip_with(first_halves, second_halves, fn x, y -> enum_compare_zip(x, y) end)
     |> Enum.concat()
-    |> Enum.map(fn x -> abc_to_number(x) end)
+    |> Enum.map(fn x -> score(x) end)
     |> Enum.sum()
   end
 
@@ -33,7 +33,7 @@ defmodule AdventOfCode.Day3 do
     read_in()
     |> Enum.chunk_every(3)
     |> Enum.map(fn x -> determine_common_letter(x) end)
-    |> Enum.map(fn x -> abc_to_number(List.first(x)) end)
+    |> Enum.map(fn x -> score(List.first(x)) end)
     |> Enum.sum()
   end
 
@@ -73,60 +73,6 @@ defmodule AdventOfCode.Day3 do
     |> Enum.uniq()
   end
 
-  defp abc_to_number(x) do
-    case x do
-      "a" -> 1
-      "b" -> 2
-      "c" -> 3
-      "d" -> 4
-      "e" -> 5
-      "f" -> 6
-      "g" -> 7
-      "h" -> 8
-      "i" -> 9
-      "j" -> 10
-      "k" -> 11
-      "l" -> 12
-      "m" -> 13
-      "n" -> 14
-      "o" -> 15
-      "p" -> 16
-      "q" -> 17
-      "r" -> 18
-      "s" -> 19
-      "t" -> 20
-      "u" -> 21
-      "v" -> 22
-      "w" -> 23
-      "x" -> 24
-      "y" -> 25
-      "z" -> 26
-      "A" -> 27
-      "B" -> 28
-      "C" -> 29
-      "D" -> 30
-      "E" -> 31
-      "F" -> 32
-      "G" -> 33
-      "H" -> 34
-      "I" -> 35
-      "J" -> 36
-      "K" -> 37
-      "L" -> 38
-      "M" -> 39
-      "N" -> 40
-      "O" -> 41
-      "P" -> 42
-      "Q" -> 43
-      "R" -> 44
-      "S" -> 45
-      "T" -> 46
-      "U" -> 47
-      "V" -> 48
-      "W" -> 49
-      "X" -> 50
-      "Y" -> 51
-      "Z" -> 52
-    end
-  end
+  defp score(<<lower::utf8>>) when lower in ?a..?z, do: lower - ?a + 1
+  defp score(<<upper::utf8>>) when upper in ?A..?Z, do: upper - ?A + 27
 end
